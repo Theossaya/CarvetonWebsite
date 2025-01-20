27b5845aa1eab016b431db8776bc7a64d4d81483
unknown <theossaya@gmail.com>
2025-01-20 09:42:46 +0100

    Update changes

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,14 @@
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navMenu = document.querySelector('nav ul');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const hamburger = document.querySelector('.hamburger');
+//     const navMenu = document.querySelector('nav ul');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('active'); // Toggle active state
-    });
+//     hamburger.addEventListener('click', () => {
+//         hamburger.classList.toggle('active'); // Toggle active state
+//     });
+// });
+
+document.querySelector('.hamburger').addEventListener('click', function () {
+    this.classList.toggle('active');
 });
